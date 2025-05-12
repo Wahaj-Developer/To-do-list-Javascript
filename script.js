9169ac564700b0a90c43c76a6bea6e188ffd6047
Web-Code <ahmedwahaj317@gmail.com>
2025-05-12 19:09:17 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 const item = document.querySelector("#item")
 const toDo = document.querySelector("#to-do")
 
+item.addEventListener(
+    "keyup",
+    function(event){
+        if (event.key == "Enter"){
+            console.log(this.value)
+            add(this.value)
+            this.value = ""
+        }
 
+    }
+)
